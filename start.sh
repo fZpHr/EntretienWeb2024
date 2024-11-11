ccc6ec9dd036a82cc3b51d2598640e2cab00b266
fZpHr <afterrush@live.fr>
2024-11-11 11:36:22 +0100

    Fix css and add script for launch

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ouvrir un terminal pour cargo watch -x run dans le répertoire axum_backend
+gnome-terminal -- bash -c "cd axum_backend && cargo watch -x run; exec bash"
+
+# Ouvrir un terminal pour npx tailwindcss -i ./css/input.css -o ./assets/tailwind.css --watch dans le répertoire dioxus_frontend
+gnome-terminal -- bash -c "cd dioxus_frontend && npx tailwindcss -i ./css/input.css -o ./assets/tailwind.css --watch; exec bash"
+
+# Ouvrir un terminal pour dx serve --hot-reload dans le répertoire dioxus_frontend
+gnome-terminal -- bash -c "cd dioxus_frontend && dx serve --hot-reload; exec bash"
